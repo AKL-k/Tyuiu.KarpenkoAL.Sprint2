1c4588b7586372bb469a2686f4677fff3681c2c1
KarpenkoArtem <TyuiuKarpenkoAL@outlook.com>
2025-09-20 23:20:15 +0500

    Task3

---
+++ Tyuiu.KarpenkoAL.Sprint2.Task3.V2.Test/DataServiceTest.cs
@@ -0,0 +1,48 @@
+﻿using Tyuiu.KarpenkoAL.Sprint2.Task3.V2.Lib;
+
+namespace Tyuiu.KarpenkoAL.Sprint2.Task3.V2.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            double x = 4;
+            double res = ds.Calculate(x);
+            double wait = -44.654;
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double res = ds.Calculate(x);
+            double wait = 1.5;
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidCondition3()
+        {
+            DataService ds = new DataService();
+            double x = 0;
+            double res = ds.Calculate(x);
+            double wait = -0.577;
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidCondition4()
+        {
+            DataService ds = new DataService();
+            double x = -16.581;
+            double res = ds.Calculate(x);
+            double wait = -182.391;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.KarpenkoAL.Sprint2.Task3.V2/Program.cs
@@ -0,0 +1,33 @@
+﻿using Tyuiu.KarpenkoAL.Sprint2.Task3.V2.Lib;
+
+Console.Title = "Спринт #2 | Выполнил: Карпенко А. Л. | ИБКСб-25-1";
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #2                                                               *");
+Console.WriteLine("* Тема: Вложенные операторы if - else                                     *");
+Console.WriteLine("* Задание #3                                                              *");
+Console.WriteLine("* Вариант #2                                                              *");
+Console.WriteLine("* Выполнил: Карпенко Артём Леонидович | ИБКСб-25-1                        *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Написать программу, которая вычисляет требуемое значение функции Y с    *");
+Console.WriteLine("* использованием вложенных операторов if-else, где пользователь вводит    *");
+Console.WriteLine("* X с клавиатуры. Округлить полученное значение до трех знаков после ,    *");
+Console.WriteLine("*                                                                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+DataService ds = new DataService();
+
+Console.WriteLine("Введите значение переменной X: ");
+double x = Convert.ToDouble(Console.ReadLine());
+double res = ds.Calculate(x);
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("Значение функции = " + res);
+
+Console.ReadKey();
