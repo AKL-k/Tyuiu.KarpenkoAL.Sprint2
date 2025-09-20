08e4a7c735458264ff27534ca3926612765d3752
KarpenkoArtem <TyuiuKarpenkoAL@outlook.com>
2025-09-20 20:47:47 +0500

    Task2

---
+++ Tyuiu.KarpenkoAL.Sprint2.Task2.V6.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.KarpenkoAL.Sprint2.Task2.V6.Lib;
+
+namespace Tyuiu.KarpenkoAL.Sprint2.Task2.V6.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCheckDotInShadedArea()
+        {
+            DataService ds = new DataService();
+            int x = 8;
+            int y = 7;
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+            bool wait = true;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.KarpenkoAL.Sprint2.Task2.V6/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.KarpenkoAL.Sprint2.Task2.V6.Lib;
+
+Console.Title = "Спринт #2 | Выполнил: Карпенко А. Л. | ИБКСб-25-1";
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #2                                                               *");
+Console.WriteLine("* Тема: Арифметические операторы в C#                                     *");
+Console.WriteLine("* Задание #2                                                              *");
+Console.WriteLine("* Вариант #6                                                              *");
+Console.WriteLine("* Выполнил: Карпенко Артём Леонидович | ИБКСб-25-1                        *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Написать программу, которая запрашивает целые значения с клавиатуры и в *");
+Console.WriteLine("* ычисляет находится ли точка с координатами X,Y в заштрихованной области *");
+Console.WriteLine("*                                                                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("Введите значение переменной X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите значение переменной Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+DataService ds = new DataService();
+bool res = ds.CheckDotInShadedArea(x, y);
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+if (res) 
+{
+    Console.WriteLine("Точка находится в заштрихованной области");
+}
+else 
+{ 
+    Console.WriteLine("Точка не находится в заштрихованной области");
+}
+
+Console.ReadKey();
